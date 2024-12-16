8d35fc2558eee671d45433937ff1b92504e5a86c
Mandraszewsky <mandrah@o2.pl>
2024-12-16 17:36:25 +0100

    two-way binding with decorators & signals

--- src/app/rect/rect.component.ts
+++ src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -9,8 +9,26 @@ import { Component } from '@angular/core';
 })
 export class RectComponent {
   // Todo: Implement custom two-way binding
+  @Input({ required: true }) size!: { width: string; height: string };
+  @Output() sizeChange = new EventEmitter<{ width: string; height: string }>(); // size (input name) + Change (it is static, angular feature), angular understand it as input + output value = 2way binding
 
   onReset() {
     // ...
+    this.sizeChange.emit({
+      width: '200',
+      height: '100'
+    });
   }
+
+  // newer and easier way using 2way binding with signals (angular 17.2 +):
+  // size = model.required<{ width: string; height: string }>();
+
+  // onReset() {
+  //   // ...
+  //   this.size.set({
+  //     width: '200',
+  //     height: '100'
+  //   });
+  // }
+
 }
